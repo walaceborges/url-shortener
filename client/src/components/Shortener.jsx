b5bfd1dee32826be831a95babd4c6523164b7c8c
Walace <walacewab@gmail.com>
2022-03-16 01:12:05 -0300

    build: adiciona o framework tailwind

--- client/src/components/Shortener.jsx
+++ client/src/components/Shortener.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 function Shortener() {
   const [url, setUrl] = useState('');
   const [newUrl, setNewUrl] = useState('');
+  
   const handleSubmit = async (e) => {
     e.preventDefault();
     const response = await fetch('http://localhost:3000', {
@@ -14,16 +15,24 @@ function Shortener() {
       body: JSON.stringify({ originalUrl: url })
     });
 
-    const parsetResponse = await response.json();
-    setNewUrl(parsetResponse.newUrl);
+    const parsedResponse = await response.json();
+    setNewUrl(parsedResponse.newUrl);
   };
+
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <input onChange={(e) => { setUrl(e.target.value)}} value={url} type="text" name="url" />
-        <button>Enter</button>
+      <form onSubmit={handleSubmit}>  
+        <input
+          className='rounded-full border-2 px-3 my-4 text-center'
+          placeholder='Paste URL to short'
+          onChange={(e) => { setUrl(e.target.value)}} 
+          value={url} 
+          type="text" 
+          name="url" 
+        />
+        <button>Encurtar</button>
       </form>
-      <p>{newUrl}</p>
+      <p className="text-red-800">{newUrl}</p>
     </div>
   )
 }

360e928dcd039d26dd29a0a08c723f0fec91bd5b
Walace <walacewab@gmail.com>
2022-03-17 01:38:26 -0300

    style: altera a estilizacao do header e adiciona os hrefs

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React from 'react';
+import Moon from "../assets/moon-svgrepo-com.svg";
 
 function Header() {
   return (
-    <nav className="flex justify-around">
-        <span class="logo">Logo</span>
-        <span>Github</span>
+    <nav className="flex justify-around pt-20">
+      <div>
+        <span class="logo font-medium text-2xl md:text-4xl">Cuttly</span>
+      </div>
+      <div className="flex items-center">
+        <img className="pr-5 md:pr-14 md:w-20" src={Moon} alt="Moon" />
+        <a 
+          href="https://github.com/walaceborges" 
+          target="_blank" 
+          rel="noreferrer" 
+          className="pr-5 md:pr-14 xl:text-xl lg:text-lg md:text-lg sm:text-xs font-normal"
+        >
+          Github
+        </a>
+        <a 
+          href="https://www.linkedin.com/in/walace-borges/" 
+          target="_blank" 
+          rel="noreferrer" 
+          className="xl:text-xl lg:text-lg md:text-lg sm:text-xs font-normal"
+        >
+          Linkedin
+        </a>
+      </div>
     </nav>
   )
 }

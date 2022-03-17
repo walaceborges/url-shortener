dab757eacfb966c62bf0d98c4850cff06545e1db
Walace <walacewab@gmail.com>
2022-03-17 12:33:17 -0300

    style: adiciona o componente main na pagina e a estilizacao do mesmo

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './Header'
+import Main from './Main'
 import Shortener from './Shortener'
 
 function Home() {
   return (
     <div className="bg-[#F5F8FF] w-screen h-screen">
       <Header />
+      <Main />
       <Shortener />
     </div>
   )
---
+++ client/src/components/Main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Main() {
+  return (
+    <main className="flex flex-col items-center pt-32">
+      <div className="pb-10">
+        <h1 className="logo text-7xl">Cuttly</h1>
+      </div>
+      <div className="2xl:w-2/5 xl:w-3/6 w-4/5">
+        <h2 className="xl:text-3xl md:text-2xl text-base text-[#626366] font-medium">
+          O encurtador de URL criado com ferramentas poderosas para ajudar você proteger seus links.</h2>
+      </div>
+    </main>
+  )
+}
+
+export default Main
